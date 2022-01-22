ee6097d9333496d20d70affdfe25db6812cc75d1
Hassan1Ehsani <hassan.ehsani.am@gmail.com>
2022-01-22 14:20:48 +0300

    create function for play all sound

--- lib/main.dart
+++ lib/main.dart
@@ -10,17 +10,59 @@ class XylophoneApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    void playSound(int soundNumber) {
+      final player = AudioCache();
+      player.play('note$soundNumber.wav');
+    }
+
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-              child: Text('Click Me'),
-            ),
+          child: Column(
+            children: [
+              FlatButton(
+                color: Colors.red,
+                onPressed: () {
+                  playSound(1);
+                },
+              ),
+              FlatButton(
+                color: Colors.orange,
+                onPressed: () {
+                  playSound(2);
+                },
+              ),
+              FlatButton(
+                color: Colors.yellow,
+                onPressed: () {
+                  playSound(3);
+                },
+              ),
+              FlatButton(
+                color: Colors.green,
+                onPressed: () {
+                  playSound(4);
+                },
+              ),
+              FlatButton(
+                color: Colors.teal,
+                onPressed: () {
+                  playSound(5);
+                },
+              ),
+              FlatButton(
+                color: Colors.blue,
+                onPressed: () {
+                  playSound(6);
+                },
+              ),
+              FlatButton(
+                color: Colors.purple,
+                onPressed: () {
+                  playSound(7);
+                },
+              ),
+            ],
           ),
         ),
       ),
